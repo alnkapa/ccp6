d1414dd19beb304e417b2f88edb0f757726bd428
Alexander A. Kapralov <alnkapa@gmail.com>
2024-07-06 16:34:20 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -1,138 +1,64 @@
-#include <algorithm>
+#include <cassert>
 #include <iostream>
-#include <vector>
+#include <map>
+#include <tuple>
 
-namespace model {
-    class Shape {
-       public:
-        Shape(std::initializer_list<float> in) : m_vertices(in){};
-        const std::vector<float> &data() const { return m_vertices; };
-        friend bool operator==(const Shape &a, const Shape &b) { return &a == &b; };
-        std::vector<unsigned char> dump() const {
-            std::vector<unsigned char> rez{};
-            // TODO
-            return rez;
-        };
-        void import(const std::vector<unsigned char> &in) {
-            // TODO
-        };
-
-       private:
-        std::vector<float> m_vertices;
-    };
-
-    class Document {
-       public:
-        void clear() { m_doc.clear(); };
-        void add_shape(Shape &&shape) { m_doc.emplace_back(shape); };
-        void add_shape(Shape &shape) { m_doc.emplace_back(shape); };
-        void del_shape(const Shape &shape) {
-            auto it = std::find(m_doc.begin(), m_doc.end(), shape);
-            if (it != m_doc.end()) {
-                m_doc.erase(it);
-            }
-        };
-        const std::vector<Shape> &data() const { return m_doc; };
-        std::vector<unsigned char> dump() const {
-            std::vector<unsigned char> rez{};
-            // TODO
-            for (const auto &v : m_doc) {
-                [[maybe_unused]] auto part = v.dump();
-                // TODO
-            }
-            // TODO
-            return rez;
-        };
-        void import(const std::vector<unsigned char> &in) {
-            // TODO
-            for (auto &v : m_doc) {
-                v.import(in);
-                // TODO
-            }
-            // TODO
-        };
-
-       private:
-        std::vector<Shape> m_doc;
-    };
-
-}  // namespace model
-
-namespace view {
-    class Shape {
-       public:
-        static void render(const std::vector<float> &shape) {
-            std::cout << "Shape: ";
-            for (const auto &v : shape) {
-                std::cout << v << " ; ";
-            }
-        };
-    };
-    class Document {
-       public:
-        static void render(const std::vector<model::Shape> &document) {
-            for (const auto &v : document) {
-                std::cout << "Document: ";
-                Shape::render(v.data());
-                std::cout << std::endl;
-            }
-        };
-    };
-}  // namespace view
-
-class Controller {
+template <typename T, int initial_value>
+class SparseMatrix {
    public:
-    void clear_document() {  // создание нового документа
-        m_model.clear();
-        view::Document::render(m_model.data());
-    };
-    void create_shape(model::Shape &&shape) {  // создание графического примитива
-        m_model.add_shape(std::move(shape));
-        view::Document::render(m_model.data());
-    };
-    void create_shape(model::Shape &shape) {  // создание графического примитива
-        m_model.add_shape(shape);
-        view::Document::render(m_model.data());
-    };
+    using result_t = SparseMatrix<T, initial_value>;
+    using key_t = std::pair<int, int>;
+    size_t size() const { return m_matrix.size(); };
+    class Proxy {
+       public:
+        Proxy(result_t &parent, int row) : m_parent(parent), m_row(row){};
+        T &operator[](int col) {
+            std::cout << "T1"
+                      << "\n";
+            return m_parent.m_matrix[{m_row, col}];
+        };
+        const T operator[](int col) const {
+            std::cout << "T2"
+                      << "\n";
+            try {
+                return m_parent.m_matrix.at({m_row, col});
+            } catch (std::out_of_range) {
+                return initial_value;
+            }
+        };
 
-    void delete_shape(const model::Shape &shape) {  // удаление графического примитива
-        m_model.del_shape(shape);
-        view::Document::render(m_model.data());
+       private:
+        int m_row;
+        result_t &m_parent;
     };
-
-    void import_document(const std::vector<unsigned char> &file) {  // импорт документа из файла
-        m_model.import(file);
-        view::Document::render(m_model.data());
+    const Proxy operator[](int row) const {
+        std::cout << "A1"
+                  << "\n";
+        return Proxy{*this, row};
     };
-    std::vector<unsigned char> export_document() {  // экспорт документа в файл
-        return m_model.dump();
+    Proxy operator[](int row) {
+        std::cout << "A1"
+                  << "\n";
+        return Proxy{*this, row};
     };
+    // const Proxy &operator[](int row) const {
+    //     std::cout << "A2"
+    //               << "\n";
+    //     const auto p = Proxy{this, row};
+    //     return p;
+    // };
 
    private:
-    model::Document m_model{};
+    std::map<key_t, T> m_matrix;
 };
 
 int main() {
-    auto c{Controller{}};
-    c.clear_document();
-    auto s1{model::Shape{{10, 20, 30, 40}}};
-    c.create_shape(s1);
-    for (int i = 0; i < 2; i++) {
-        c.create_shape(std::move(model::Shape{{11, 12}}));
-    };
-    c.delete_shape(s1);
-    const auto data = c.export_document();
-    for (int i = 0; i < 3; i++) {
-        c.create_shape(std::move(model::Shape{{15, 3}}));
-    };
-    c.import_document(data);
-    // TODO
+    SparseMatrix<int, -1> matrix;
+    assert(matrix.size() == 0);  // все ячейки свободны
+    auto const a = matrix[0][0];
+    // // assert(a == -1);
+    // // assert(matrix.size() == 0);
+    matrix[100][100] = 314;
+    assert(matrix[100][100] == 314);
     return 0;
 }
-/*
-строки ведут себя как вектор ?
-
-а есть что то для итераторов для из инвалидации
-автоматический
-
-*/
