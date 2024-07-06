1d771b9c46a7feda35da7439c5bb77e8c2a112ad
Alexander A. Kapralov <alnkapa@gmail.com>
2024-07-06 23:10:50 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ class Matrix {
         T &operator[](int col) {
             std::cout << "T1"
                       << "\n";
-            std::cout << m_row[col] << "\n";          
+            std::cout << m_row[col] << "\n";
             return m_row[col];
         };
         bool dd() { return true; };
@@ -42,15 +42,58 @@ class Matrix {
         }
     };
 };
+struct Arg {
+    int i = 1;
+    int &value() &  // non-const lvalue
+    {
+        std::cout << __PRETTY_FUNCTION__ << "\n";
+        return i;
+    }
 
+    int &&value() &&  // non-const rvalue
+    {
+        std::cout << __PRETTY_FUNCTION__ << "\n";
+        return std::move(i);  // propagate rvalue-ness
+    }
+
+    const int &value() const &  // const lvalue
+    {
+        std::cout << __PRETTY_FUNCTION__ << "\n";
+        return i;
+    }
+
+    const int &&value() const &&  // const rvalue
+    {
+        std::cout << __PRETTY_FUNCTION__ << "\n";
+        return std::move(i);  // propagate rvalue-ness
+    }
+};
+template <class T>
+void wrapper(T &&arg) {
+    foo(forward<decltype(forward<T>(arg).get())>(forward<T>(arg).get()));
+}
 int main() {
-    Matrix<int, -1> matrix;
-    assert(matrix.size() == 0);  // все ячейки свободны
-    auto a = matrix[0][0];
-    assert(a == -1);
-    assert(matrix.size() == 0);
-    matrix[100][100] = 314;
-    assert(matrix[100][100] == 314);
-    assert(matrix.size() == 1);
+    Arg m;
+    auto a = m.value();
+    m.value() = a;
+    const Arg m1;
+    auto b = m1.value();
+    // m1.value() = a;
+    auto d = Arg().value();  // вызов non-const rvalue
+
+    // Arg().value() = 5;  // вызов non-const rvalue
+
+    auto const d1 = Arg().value();  // вызов const rvalue
+
     return 0;
 }
+// Matrix<int, -1> matrix;
+// assert(matrix.size() == 0);  // все ячейки свободны
+// auto a = matrix[0][0];
+// assert(a == -1);
+// assert(matrix.size() == 0);
+// matrix[100][100] = 314;
+// assert(matrix[100][100] == 314);
+// assert(matrix.size() == 1);
+// return 0;
+// }
