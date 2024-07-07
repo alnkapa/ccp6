e76d5813c622373f39eede97392aeff80d626f8d
Alexander A. Kapralov <alnkapa@gmail.com>
2024-07-07 18:55:21 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -1,112 +1,80 @@
 #include <algorithm>
 #include <cassert>
 #include <iostream>
-#include <unordered_map>
+#include <map>
 
 template <typename T, int initial_value>
 class Matrix {
-    friend class Proxy;
-
    private:
-    std::size_t m_size{0};
-    std::unordered_map<int, std::unordered_map<int, T>> m_matrix;
-
-   public:
-    size_t size() const { return m_size; };
+    using key_t = std::pair<int, int>;
+    using type_t = std::map<key_t, T>;
+    type_t m_matrix;
     class Proxy {
        private:
-        std::unordered_map<int, T> &m_row;
+        type_t &m_matrix;
+        int m_row;
+        int m_col;
 
        public:
-        Proxy(std::unordered_map<int, T> &row) : m_row(row){};
-        T &operator[](int col) {
-            std::cout << "T1"
-                      << "\n";
-            std::cout << m_row[col] << "\n";
-            return m_row[col];
-        };
-        bool dd() { return true; };
-    };
-    Proxy operator[](int row) {
-        std::cout << "A1"
-                  << "\n";
-        if (auto it = m_matrix.find(row); it != m_matrix.end()) {
-            std::cout << "it1"
-                      << "\n";
-            return Proxy{it->second};
-        } else {
-            auto m = Proxy{m_matrix[row]};
-            m.dd();
-            std::cout << "n:" << m_matrix.size() << std::endl;
-            return m;
-        }
-    };
-};
-struct Arg {
-    struct In {
-        int i;
-        int operator=(int in) {
-            std::cout << __PRETTY_FUNCTION__ << "\n";
-            return i;
-        };
-        In &operator=(const In &in) {
-            std::cout << __PRETTY_FUNCTION__ << "\n";
+        explicit Proxy(type_t &matrix, int row) : m_matrix(matrix), m_row(row){};
+        Proxy &operator[](int col) {
+            m_col = col;
             return *this;
         };
-        const In &operator=(const In &in) const  {
-            std::cout << __PRETTY_FUNCTION__ << "\n";
+        Proxy &operator=(T in) {
+            m_matrix[key_t{m_row, m_col}] = in;
             return *this;
         };
+        operator T() const {
+            if (auto it = m_matrix.find(key_t{m_row, m_col}); it != m_matrix.end()) {
+                return it->second;
+            } else {
+                return initial_value;
+            }
+        };
     };
-    In i;
-    In &value() &  // non-const lvalue
-    {
-        std::cout << __PRETTY_FUNCTION__ << "\n";
-        return i;
-    }
+    class Iterator {
+       private:
+        type_t::iterator m_ptr;
 
-    In &&value() &&  // non-const rvalue
-    {
-        std::cout << __PRETTY_FUNCTION__ << "\n";
-        return std::move(i);  // propagate rvalue-ness
-    }
+       public:
+        using iterator_category = std::input_iterator_tag;
+        explicit Iterator(type_t::iterator ptr) : m_ptr(ptr){};
+        Iterator &operator++() {
+            m_ptr++;
+            return *this;
+        };
+        std::tuple<int, int, T> operator*() {
+            return std::make_tuple((m_ptr->first).first, (m_ptr->first).second, m_ptr->second);
+        };
+        friend bool operator!=(const Iterator &a, const Iterator &b) { return a.m_ptr != b.m_ptr; };
+    };
 
-    const In &value() const &  // const lvalue
-    {
-        std::cout << __PRETTY_FUNCTION__ << "\n";
-        return i;
-    }
+   public:
+    std::size_t size() const { return m_matrix.size(); };
+    Proxy operator[](int row) { return Proxy{m_matrix, row}; };
 
-    const In &&value() const &&  // const rvalue
-    {
-        std::cout << __PRETTY_FUNCTION__ << "\n";
-        return std::move(i);  // propagate rvalue-ness
-    }
+    Iterator begin() { return Iterator{m_matrix.begin()}; };
+    Iterator end() { return Iterator{m_matrix.end()}; };
 };
-
 int main() {
-    Arg m;
-    m.value() = 1;
-    auto a = m.value();
-    //m.value() = a;
-    // const Arg m1{};
-    // auto b = m1.value();
-    // // m1.value() = a;
-    // auto d = Arg().value();  // вызов non-const rvalue
+    Matrix<int, -1> matrix;
+    assert(matrix.size() == 0);  // все ячейки свободны
+    auto a = matrix[0][0];
+    assert(a == -1);
+    assert(matrix.size() == 0);
+    matrix[100][100] = 314;
+    assert(matrix[100][100] == 314);
+    assert(matrix.size() == 1);
 
-    // // Arg().value() = 5;  // вызов non-const rvalue
-
-    // auto const d1 = Arg().value();  // вызов const rvalue
-
+    // выведется одна строка
+    // 100100314
+    for (auto c : matrix) {
+        int x;
+        int y;
+        int v;
+        std::tie(x, y, v) = c;
+        std::cout << x << y << v << std::endl;
+    }
     return 0;
 }
-// Matrix<int, -1> matrix;
-// assert(matrix.size() == 0);  // все ячейки свободны
-// auto a = matrix[0][0];
-// assert(a == -1);
-// assert(matrix.size() == 0);
-// matrix[100][100] = 314;
-// assert(matrix[100][100] == 314);
-// assert(matrix.size() == 1);
-// return 0;
-// }
